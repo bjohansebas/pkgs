d7a815ba3d7b4bdbf5e98ab8745b1c599292e0c0
Sebastian Beltran <bjohansebas@gmail.com>
2024-01-30 21:13:06 -0500

    chore: remove scanner package

--- packages/scanner/src/helpers/get-package-manager.ts
+++
@@ -1,8 +0,0 @@
-import { PackageManager } from '../types'
-
-export function getPackageManager(files: string[]): PackageManager | undefined {
-  if (files.includes('pnpm-lock.yaml')) return 'pnpm'
-  if (files.includes('package-lock.json')) return 'npm'
-  if (files.includes('bun.lockb')) return 'bun'
-  if (files.includes('yarn.lock')) return 'yarn'
-}
