119c8f2a456b7f37eadb3fc5af581d91c1e7729e
Sebastian Beltran <bjohansebas@gmail.com>
2024-03-26 15:48:26 -0500

    add options for add command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander'
+import { Command, Option } from 'commander'
 import packageJson from '../package.json'
 
 const program = new Command(packageJson.name).version(packageJson.version)
+
+program
+  .command('add')
+  .addOption(new Option('--formatter, -f').choices(['biome', 'prettier']))
+  .addOption(new Option('--linter, -l').choices(['eslint', 'oxc', 'biome']))
+  .addOption(new Option('--git-hooks, -gh').choices(['husky', 'lefthook']))
+
+program.allowUnknownOption().parse(process.argv)
