9d8a8fb8f22f6d77f838d038064866f47e5276ff
Sebastian Beltran <bjohansebas@gmail.com>
2024-03-27 22:36:17 -0500

    chore(scanner): remove bin for build

--- packages/scanner/tsup.config.ts
+++ packages/scanner/tsup.config.ts
@@ -3,7 +3,6 @@ import { type Options, defineConfig } from 'tsup'
 export default defineConfig((options: Options) => ({
   entry: {
     helpers: 'src/helpers/index.ts',
-    bin: 'src/bin.ts',
   },
   minify: true,
   format: ['cjs', 'esm'],
