9eb03626296f843d545ddc462cff1bffecd6f1f9
Sebastian Beltran <bjohansebas@gmail.com>
2024-07-02 13:41:11 -0500

    test(scanner): remove some biome test

--- packages/scanner/tests/linters/biome/biome.test.ts
+++ packages/scanner/tests/linters/biome/biome.test.ts
@@ -1,6 +1,5 @@
-import path from 'node:path'
 import { describe, expect, it } from 'vitest'
-import { generateReport, scanFolder } from '../../../src'
+import { generateReport } from '../../../src'
 import { biomeFiles } from '../../../src/constants'
 
 describe('report biome when exists file', () => {
@@ -30,38 +29,5 @@ describe('report biome when exists file', () => {
     const report = generateReport(['src/lbiome.json', 'index.ts'])
 
     expect(report.linters).toBe(null)
-  })
-})
-
-describe('read the file', () => {
-  it('do not report biome when linter is desactived', async () => {
-    const resolvedProjectPath = path.resolve('./biome_not_linter')
-
-    const files = await scanFolder(resolvedProjectPath)
-
-    const report = generateReport(files)
-
-    expect(report.linters).toBe(null)
-  })
-
-  it("do not report biome when biome isn't dependencie", async () => {
-    const resolvedProjectPath = path.resolve('./biome_not_installed')
-
-    const files = await scanFolder(resolvedProjectPath)
-
-    const report = generateReport(files)
-
-    expect(report.linters).toBe(null)
-  })
-
-  it('good config', async () => {
-    const resolvedProjectPath = path.resolve('./correct_config')
-
-    const files = await scanFolder(resolvedProjectPath)
-
-    const report = generateReport(files)
-
-    expect(report.linters).toHaveLength(1)
-    expect(report.linters).toContain('biome')
   })
 })
